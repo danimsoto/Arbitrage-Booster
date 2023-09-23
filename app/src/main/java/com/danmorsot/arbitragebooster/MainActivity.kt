cf794bacbad40e0d5e3c40c0e104c7dae4a2e61f
Dani Moreno <v.daniel.moreno@gmail.com>
2023-09-23 20:15:13 +0200

    added DaggerHilt

--- app/src/main/java/com/danmorsot/arbitragebooster/MainActivity.kt
+++ app/src/main/java/com/danmorsot/arbitragebooster/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.danmorsot.arbitragebooster.ui.navigation.AppNavigation
 import com.danmorsot.arbitragebooster.ui.theme.ArbitrageBoosterTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
